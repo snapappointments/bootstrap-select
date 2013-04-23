8b3bbd22197a9596af013154cae84e1b0805b486
caseyjhol <casey17648@gmail.com>
2013-04-23 17:01:10 -0600

    Update bootstrap-select.js

--- javascripts/bootstrap-select.js
+++ javascripts/bootstrap-select.js
@@ -71,7 +71,7 @@
         createDropdown: function() {
             var drop =
                 "<div class='btn-group bootstrap-select'>" +
-                    "<button class='btn dropdown-toggle clearfix' data-toggle='dropdown'>" +
+                    "<button type='button' class='btn dropdown-toggle clearfix' data-toggle='dropdown'>" +
                         "<span class='filter-option pull-left'></span>&nbsp;" +
                         "<span class='caret'></span>" +
                     "</button>" +
@@ -118,16 +118,21 @@
                 var optionClass = $(this).attr("class") !== undefined ? $(this).attr("class") : '';
                 var text =  $(this).text();
                 var subtext = $(this).data('subtext') !== undefined ? '<small class="muted">'+$(this).data('subtext')+'</small>' : '';
+                var icon = $(this).data('icon') !== undefined ? '<i class="'+$(this).data('icon')+'"></i> ' : '';
+                if ($(this).is(':disabled') || $(this).parent().is(':disabled')) {
+                    icon = '<span>'+icon+'</span>';
+                }
                 
-                //Append any subtext to the main text.
-                text+=subtext;
+                //Prepend any icon and append any subtext to the main text.
+                 text = icon + '<span class="text">' + text + subtext + '</span>';
 
                 if ($(this).parent().is('optgroup') && $(this).data('divider') != true) {
                     if ($(this).index() == 0) {
                         //Get the opt group label
                         var label = $(this).parent().attr('label');
                         var labelSubtext = $(this).parent().data('subtext') !== undefined ? '<small class="muted">'+$(this).parent().data('subtext')+'</small>' : '';
-                        label += labelSubtext;
+                        var labelIcon = $(this).parent().data('icon') ? '<i class="'+$(this).parent().data('icon')+'"></i> ' : '';
+                        label = labelIcon + '<span class="text">' + label + labelSubtext + '</span>';
                   
                         if ($(this)[0].index != 0) {
                             _liA.push(
@@ -165,12 +170,11 @@
             return $(_liHtml);
         },
         
-        createA:function(test, classes) {
+        createA:function(text, classes) {
          return '<a tabindex="-1" href="#" class="'+classes+'">' +
-                 '<span class="pull-left">' + test + '</span>' +
+                 text +
                  '<i class="icon-ok check-mark"></i>' + 
                  '</a>';
-                
         },
         
         render:function() {
@@ -294,9 +298,13 @@
                 this.$newElement.find('li').eq(index).removeClass('disabled');
             }
         },
+
+        isDisabled: function() {
+                return this.$element.is(':disabled') || this.$element.attr('readonly');
+        },
        
         checkDisabled: function() {
-            if (this.$element.is(':disabled')) {
+            if (this.isDisabled()) {
                 this.button.addClass('disabled');
                 this.button.click(function(e) {
                     e.preventDefault();
@@ -304,7 +312,7 @@
                 this.button.on('focusin', function() {
                     $(this).blur();
                 });
-            } else if (!this.$element.is(':disabled') && this.button.hasClass('disabled')) {
+            } else if (!this.isDisabled() && this.button.hasClass('disabled')) {
                 this.button.removeClass('disabled');
                 this.button.click(function() {
                     return true;
@@ -451,7 +459,8 @@
         selectedTextFormat : 'values',
         noneSelectedText : 'Nothing selected',
         width: null,
-        container: false
+        container: false,
+        icon: false
     }
 
 }(window.jQuery);
