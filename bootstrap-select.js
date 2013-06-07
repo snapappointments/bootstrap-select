3add98fb96e90143862d6848e449272974ff53f8
caseyjhol <casey17648@gmail.com>
2013-06-07 17:10:03 -0500

    Fix #155 (properly)
    The new changes fixed #165, but broke #155 again. The issue was occurring in Firefox when a select already had an option selected by default, selected="selected". This was causing issues with `prevValue` and thus a change event wasn't occurring when it should have been. This now only performs `removeAttr()` on items items that are already set to `selected="selected` which for some reason works.

--- bootstrap-select.js
+++ bootstrap-select.js
@@ -377,7 +377,7 @@
                 if (_this.$element.not(':disabled') && !$(this).parent().hasClass('disabled')){
                     //Deselect all others if not multi select box
                     if (!_this.multiple) {
-                        _this.$element.find('option').removeAttr('selected').prop('selected', false);
+                        _this.$element.find('option').prop('selected',true).removeAttr('selected');
                         _this.$element.find('option').eq(clickedIndex).prop('selected', true).attr('selected', 'selected');
                     }
                     //Else toggle the one we have chosen if we are multi select.
