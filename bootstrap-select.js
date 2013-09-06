3775954d93988f5473b6acdf26303bd9eea80db0
Silvio Moreto <silviomoreto@gmail.com>
2013-09-06 18:36:31 -0300

    demo/docs added search and ad

--- bootstrap-select.js
+++ bootstrap-select.js
@@ -1,5 +1,5 @@
 /*!
- * bootstrap-select v1.2.0
+ * bootstrap-select v1.3.1
  * http://silviomoreto.github.io/bootstrap-select/
  *
  * Copyright 2013 bootstrap-select
@@ -9,6 +9,12 @@
 !function($) {
 
     "use strict";
+
+    $.expr[":"].icontains = $.expr.createPseudo(function(arg) {
+        return function( elem ) {
+            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+        };
+    });
 
     var Selectpicker = function(element, options, e) {
         if (e) {
@@ -43,6 +49,8 @@
         constructor: Selectpicker,
 
         init: function(e) {
+
+            console.log('vaca!')
             this.$element.hide();
             this.multiple = this.$element.prop('multiple');
             var id = this.$element.attr('id');
@@ -50,18 +58,20 @@
             this.$element.after(this.$newElement);
             this.$menu = this.$newElement.find('> .dropdown-menu');
             this.$button = this.$newElement.find('> button');
+            this.$searchbox = this.$newElement.find('input');
 
             if (id !== undefined) {
-                var _this = this;
+                var that = this;
                 this.$button.attr('data-id', id);
                 $('label[for="' + id + '"]').click(function() {
-                    _this.$button.focus();
+                    that.$button.focus();
                 });
             }
 
             this.checkDisabled();
             this.checkTabIndex();
             this.clickListener();
+            this.liveSearchListener();
             this.render();
             this.liHeight();
             this.setStyle();
@@ -74,17 +84,19 @@
         },
 
         createDropdown: function() {
-            //If we are multiple, then add the show-tick class by default            
+            //If we are multiple, then add the show-tick class by default
             var multiple = this.multiple ? ' show-tick' : '';
             var header = this.options.header ? '<h3 class="popover-title">' + this.options.header + '<button type="button" class="close" aria-hidden="true">&times;</button></h3>' : '';
+            var searchbox = this.options.liveSearch ? '<div class="bootstrap-select-searchbox"><input type="text" class="input-block-level" /></div>' : '';
             var drop =
                 "<div class='btn-group bootstrap-select" + multiple + "'>" +
-                    "<button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>" +
+                    "<button type='button' class='btn dropdown-toggle' data-toggle='dropdown'>" +
                         "<div class='filter-option pull-left'></div>&nbsp;" +
                         "<div class='caret'></div>" +
                     "</button>" +
                     "<div class='dropdown-menu open'>" +
                         header +
+                        searchbox +
                         "<ul class='dropdown-menu inner' role='menu'>" +
                         "</ul>" +
                     "</div>" +
@@ -113,7 +125,7 @@
         },
 
         createLi: function() {
-            var _this = this,
+            var that = this,
                 _liA = [],
                 _liHtml = '';
 
@@ -135,7 +147,7 @@
                     text = icon + '<span class="text">' + text + subtext + '</span>';
                 }
 
-                if (_this.options.hideDisabled && ($this.is(':disabled') || $this.parent().is(':disabled'))) {
+                if (that.options.hideDisabled && ($this.is(':disabled') || $this.parent().is(':disabled'))) {
                     _liA.push('<a style="min-height: 0; padding: 0"></a>');
                 } else if ($this.parent().is('optgroup') && $this.data('divider') != true) {
                     if ($this.index() == 0) {
@@ -149,22 +161,22 @@
                             _liA.push(
                                 '<div class="div-contain"><div class="divider"></div></div>'+
                                 '<dt>'+label+'</dt>'+
-                                _this.createA(text, "opt " + optionClass, inline )
+                                that.createA(text, "opt " + optionClass, inline )
                                 );
                         } else {
                             _liA.push(
                                 '<dt>'+label+'</dt>'+
-                                _this.createA(text, "opt " + optionClass, inline ));
+                                that.createA(text, "opt " + optionClass, inline ));
                         }
                     } else {
-                         _liA.push( _this.createA(text, "opt " + optionClass, inline ) );
+                         _liA.push(that.createA(text, "opt " + optionClass, inline ));
                     }
                 } else if ($this.data('divider') == true) {
                     _liA.push('<div class="div-contain"><div class="divider"></div></div>');
                 } else if ($(this).data('hidden') == true) {
                     _liA.push('');
                 } else {
-                    _liA.push( _this.createA(text, optionClass, inline ) );
+                    _liA.push(that.createA(text, optionClass, inline ));
                 }
             });
 
@@ -173,7 +185,7 @@
             });
 
             //If we are not multiple, and we dont have a selected item, and we dont have a title, select the first element so something is set in the button
-            if (!this.multiple && this.$element.find('option:selected').length==0 && !_this.options.title) {
+            if (!this.multiple && this.$element.find('option:selected').length==0 && !this.options.title) {
                 this.$element.find('option').eq(0).prop('selected', true).attr('selected', 'selected');
             }
 
@@ -188,24 +200,24 @@
         },
 
         render: function() {
-            var _this = this;
+            var that = this;
 
             //Update the LI to match the SELECT
             this.$element.find('option').each(function(index) {
-               _this.setDisabled(index, $(this).is(':disabled') || $(this).parent().is(':disabled') );
-               _this.setSelected(index, $(this).is(':selected') );
+               that.setDisabled(index, $(this).is(':disabled') || $(this).parent().is(':disabled') );
+               that.setSelected(index, $(this).is(':selected') );
             });
 
             var selectedItems = this.$element.find('option:selected').map(function(index,value) {
                 var $this = $(this);
-                var icon = $this.data('icon') && _this.options.showIcon ? '<i class="glyphicon ' + $this.data('icon') + '"></i> ' : '';
+                var icon = $this.data('icon') && that.options.showIcon ? '<i class="glyphicon ' + $this.data('icon') + '"></i> ' : '';
                 var subtext;
-                if (_this.options.showSubtext && $this.attr('data-subtext') && !_this.multiple) {
+                if (that.options.showSubtext && $this.attr('data-subtext') && !that.multiple) {
                     subtext = ' <small class="muted">'+$this.data('subtext') +'</small>';
                 } else {
                     subtext = '';
                 }
-                if ($this.data('content') && _this.options.showContent) {
+                if ($this.data('content') && that.options.showContent) {
                     return $this.data('content');
                 } else if ($this.attr('title') != undefined) {
                     return $this.attr('title');
@@ -219,20 +231,20 @@
             var title = !this.multiple ? selectedItems[0] : selectedItems.join(", ");
 
             //If this is multi select, and the selectText type is count, the show 1 of 2 selected etc..
-            if (_this.multiple && _this.options.selectedTextFormat.indexOf('count') > -1) {
-                var max = _this.options.selectedTextFormat.split(">");
+            if (this.multiple && this.options.selectedTextFormat.indexOf('count') > -1) {
+                var max = this.options.selectedTextFormat.split(">");
                 var notDisabled = this.options.hideDisabled ? ':not([disabled])' : '';
                 if ( (max.length>1 && selectedItems.length > max[1]) || (max.length==1 && selectedItems.length>=2)) {
-                    title = _this.options.countSelectedText.replace('{0}', selectedItems.length).replace('{1}', this.$element.find('option:not([data-divider="true"]):not([data-hidden="true"])'+notDisabled).length);
+                    title = this.options.countSelectedText.replace('{0}', selectedItems.length).replace('{1}', this.$element.find('option:not([data-divider="true"]):not([data-hidden="true"])'+notDisabled).length);
                 }
              }
 
             //If we dont have a title, then use the default, or if nothing is set at all, use the not selected text
             if (!title) {
-                title = _this.options.title != undefined ? _this.options.title : _this.options.noneSelectedText;
+                title = this.options.title != undefined ? this.options.title : this.options.noneSelectedText;
             }
 
-            _this.$newElement.find('.filter-option').html(title);
+            this.$newElement.find('.filter-option').html(title);
         },
 
         setStyle: function(style, status) {
@@ -263,7 +275,7 @@
         },
 
         setSize: function() {
-            var _this = this,
+            var that = this,
                 menu = this.$menu,
                 menuInner = menu.find('.inner'),
                 menuA = menuInner.find('li > a'),
@@ -282,19 +294,19 @@
                 selectOffsetTop,
                 selectOffsetBot,
                 posVert = function() {
-                    selectOffsetTop = _this.$newElement.offset().top - $window.scrollTop();
+                    selectOffsetTop = that.$newElement.offset().top - $window.scrollTop();
                     selectOffsetBot = $window.height() - selectOffsetTop - selectHeight;
                 };
                 posVert();
                 if (this.options.header) menu.css('padding-top', 0);
-                
+
             if (this.options.size == 'auto') {
                 var getSize = function() {
                     var minHeight;
                     posVert();
                     menuHeight = selectOffsetBot - menuExtras;
-                    _this.$newElement.toggleClass('dropup', (selectOffsetTop > selectOffsetBot) && (menuHeight - menuExtras) < menu.height() && _this.options.dropupAuto);
-                    if (_this.$newElement.hasClass('dropup')) {
+                    that.$newElement.toggleClass('dropup', (selectOffsetTop > selectOffsetBot) && (menuHeight - menuExtras) < menu.height() && that.options.dropupAuto);
+                    if (that.$newElement.hasClass('dropup')) {
                         menuHeight = selectOffsetTop - menuExtras;
                     }
                     if ((menu.find('li').length + menu.find('dt').length) > 3) {
@@ -348,7 +360,7 @@
         },
 
         selectPosition: function() {
-            var _this = this,
+            var that = this,
                 drop = "<div />",
                 $drop = $(drop),
                 pos,
@@ -361,18 +373,18 @@
                 };
             this.$newElement.on('click', function(e) {
                 getPlacement($(this));
-                $drop.appendTo(_this.options.container);
+                $drop.appendTo(that.options.container);
                 $drop.toggleClass('open', !$(this).hasClass('open'));
-                $drop.append(_this.$menu);
+                $drop.append(that.$menu);
             });
             $(window).resize(function() {
-                getPlacement(_this.$newElement);
+                getPlacement(that.$newElement);
             });
             $(window).on('scroll', function(e) {
-                getPlacement(_this.$newElement);
+                getPlacement(that.$newElement);
             });
             $('html').on('click', function(e) {
-                if ($(e.target).closest(_this.$newElement).length < 1) {
+                if ($(e.target).closest(that.$newElement).length < 1) {
                     $drop.removeClass('open');
                 }
             });
@@ -389,6 +401,7 @@
             this.setWidth();
             this.setStyle();
             this.checkDisabled();
+            this.liHeight();
         },
 
         setSelected: function(index, selected) {
@@ -408,7 +421,7 @@
         },
 
         checkDisabled: function() {
-            var _this = this;
+            var that = this;
             if (this.isDisabled()) {
                 this.$button.addClass('disabled');
                 this.$button.attr('tabindex','-1');
@@ -417,7 +430,7 @@
                 this.$button.removeAttr('tabindex');
             }
             this.$button.click(function() {
-                return !_this.isDisabled();
+                return !that.isDisabled();
             });
         },
 
@@ -429,35 +442,35 @@
         },
 
         clickListener: function() {
-            var _this = this;
+            var that = this;
 
             $('body').on('touchstart.dropdown', '.dropdown-menu', function(e) {
                 e.stopPropagation();
             });
 
             this.$newElement.on('click', function() {
-                _this.setSize();
+                that.setSize();
             });
 
             this.$menu.on('click', 'li a', function(e) {
                 var clickedIndex = $(this).parent().index(),
                     $this = $(this).parent(),
-                    prevValue = _this.$element.val();
+                    prevValue = that.$element.val();
 
                 //Dont close on multi choice menu
-                if (_this.multiple) {
+                if (that.multiple) {
                     e.stopPropagation();
                 }
 
                 e.preventDefault();
 
                 //Dont run if we have been disabled
-                if (!_this.isDisabled() && !$(this).parent().hasClass('disabled')) {
-                    var $options = _this.$element.find('option');
+                if (!that.isDisabled() && !$(this).parent().hasClass('disabled')) {
+                    var $options = that.$element.find('option');
                     var $option = $options.eq(clickedIndex);
 
                     //Deselect all others if not multi select box
-                    if (!_this.multiple) {
+                    if (!that.multiple) {
                         $options.prop('selected', false);
                         $option.prop('selected', true);
                     }
@@ -468,11 +481,11 @@
                         $option.prop('selected', !state);
                     }
 
-                    _this.$button.focus();
+                    that.$button.focus();
 
                     // Trigger select 'change'
-                    if (prevValue != _this.$element.val()) {
-                        _this.$element.change();
+                    if (prevValue != that.$element.val()) {
+                        that.$element.change();
                     }
                 }
             });
@@ -481,12 +494,32 @@
                 if (e.target == this) {
                     e.preventDefault();
                     e.stopPropagation();
-                    _this.$button.focus();
+                    that.$button.focus();
                 }
             });
 
+            this.$searchbox.on('click', function(e) {
+                e.stopPropagation();
+            });
+
             this.$element.change(function() {
-                _this.render()
+                that.render()
+            });
+        },
+
+        liveSearchListener: function() {
+            var that = this;
+
+            this.$newElement.on('click.dropdown.data-api', function(e){
+                if(that.options.liveSearch) {
+                    setTimeout(function() {
+                        that.$searchbox.focus();
+                    }, 10);
+                }
+            });
+
+            this.$searchbox.on('input', function() {
+                that.$newElement.find('li').show().not(':icontains(' + that.$searchbox.val() + ')').hide();
             });
         },
 
@@ -666,7 +699,8 @@
         showIcon: true,
         showContent: true,
         dropupAuto: true,
-        header: false
+        header: false,
+        liveSearch: false
     }
 
     $(document)
