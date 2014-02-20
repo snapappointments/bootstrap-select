44fde9f06cb5302d2c9a3d48531f017c041d5c79
Rodrigo de Avila <rodrigo@avila.net.br>
2014-02-20 11:55:29 -0300

    Added pt_BR default strings
    Added files to translate default messages to Brazilian Portuguese, as
    suggested on silviomoreto/bootstrap-select#473

---
+++ i18n/defaults-pt_BR.js
@@ -0,0 +1,12 @@
+/*
+ * Translated default messages for bootstrap-select.
+ * Locale: PT (Portuguese; português)
+ * Region: BR (Brazil)
+ */
+( function($) {
+	$.extend($.fn.selectpicker.defaults = {
+		noneSelectedText : 'Nada selecionado',
+		noneResultsText : 'Nada encontrado contendo',
+		countSelectedText : 'Selecionado {0} de {1}'
+	});
+}(jQuery)); 
