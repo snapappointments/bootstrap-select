8246e4a5ad5f6585b5ac37745170499993896d0a
Weylin-fr <xavier.giraud@gmx.com>
2014-03-11 15:29:19 +0100

    Update bootstrap-select.js

--- bootstrap-select.js
+++ bootstrap-select.js
@@ -699,8 +699,8 @@
             if (value !== undefined) {
                 
                 //multipleselect support (works perfectly)
-                if(this.multiple) {
-                    var previous_values = this.$element.val();
+                var previous_values = this.$element.val();
+                if(this.multiple && previous_values) {
 
                     //If not selected : select. Else unselect
                     if(jQuery.inArray(value, previous_values)) {
