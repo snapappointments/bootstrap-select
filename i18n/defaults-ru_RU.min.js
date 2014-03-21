2b6faf9cf7180945a8ed6cb6bd63e21808e5fc0f
Silvio Moreto <silviomoreto@gmail.com>
2014-03-21 13:56:41 -0300

    Create defaults-ru_RU.min.js

---
+++ i18n/defaults-ru_RU.min.js
@@ -0,0 +1,6 @@
+/*
+ * Translated default messages for bootstrap-select.
+ * Locale: RU (Russian; Ñ€ÑƒÑÑÐºÐ¸Ð¹)
+ * Region: RU (Russian Federation)
+ */
+(function(e){e.fn.selectpicker.defaults={style:"btn-default",size:"auto",title:null,selectedTextFormat:"values",noneSelectedText:"ÐÐ¸Ñ‡ÐµÐ³Ð¾ Ð½Ðµ Ð²Ñ‹Ð±Ñ€Ð°Ð½Ð¾",noneResultsText:"ÐÐµ Ð½ÐµÐ¹Ð´ÐµÐ½Ð¾ ÑÐ¾Ð²Ð¿Ð°Ð´ÐµÐ½Ð¸Ð¹",countSelectedText:"Ð’Ñ‹Ð±Ñ€Ð°Ð½Ð¾ {0} Ð¸Ð· {1}",maxOptionsText:["Ð”Ð¾ÑÑ‚Ð¸Ð³Ð½ÑƒÑ‚ Ð¿Ñ€ÐµÐ´ÐµÐ» ({n} {var} Ð¼Ð°ÐºÑÐ¸Ð¼ÑƒÐ¼)","Ð”Ð¾ÑÑ‚Ð¸Ð³Ð½ÑƒÑ‚ Ð¿Ñ€ÐµÐ´ÐµÐ» Ð² Ð³Ñ€ÑƒÐ¿Ð¿Ðµ ({n} {var} Ð¼Ð°ÐºÑÐ¸Ð¼ÑƒÐ¼)",["items","item"]],width:false,container:false,hideDisabled:false,showSubtext:false,showIcon:true,showContent:true,dropupAuto:true,header:false,liveSearch:false,actionsBox:false,multipleSeparator:", ",iconBase:"glyphicon",tickIcon:"glyphicon-ok",maxOptions:false}})(jQuery)
