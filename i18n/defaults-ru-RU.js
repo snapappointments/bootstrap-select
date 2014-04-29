d74542125d288b462c8ac5ef038a0705e47f7d9d
Silvio Moreto <silviomoreto@gmail.com>
2014-04-29 09:08:05 -0300

    Update defaults-ru-RU.js

--- i18n/defaults-ru-RU.js
+++ i18n/defaults-ru-RU.js
@@ -1,18 +1,18 @@
 /*
  * Translated default messages for bootstrap-select.
- * Locale: RU (Russian; Ñ€ÑƒÑÑÐºÐ¸Ð¹)
+ * Locale: RU (Russian; Русский)
  * Region: RU (Russian Federation)
  */
 (function($) {
-	$.fn.selectpicker.defaults = {
+    $.fn.selectpicker.defaults = {
       style: 'btn-default',
       size: 'auto',
       title: null,
       selectedTextFormat : 'values',
-      noneSelectedText : 'ÐÐ¸Ñ‡ÐµÐ³Ð¾ Ð½Ðµ Ð²Ñ‹Ð±Ñ€Ð°Ð½Ð¾',
-	    noneResultsText : 'ÐÐµ Ð½ÐµÐ¹Ð´ÐµÐ½Ð¾ ÑÐ¾Ð²Ð¿Ð°Ð´ÐµÐ½Ð¸Ð¹',
-		  countSelectedText : 'Ð’Ñ‹Ð±Ñ€Ð°Ð½Ð¾ {0} Ð¸Ð· {1}',
-      maxOptionsText: ['Ð”Ð¾ÑÑ‚Ð¸Ð³Ð½ÑƒÑ‚ Ð¿Ñ€ÐµÐ´ÐµÐ» ({n} {var} Ð¼Ð°ÐºÑÐ¸Ð¼ÑƒÐ¼)', 'Ð”Ð¾ÑÑ‚Ð¸Ð³Ð½ÑƒÑ‚ Ð¿Ñ€ÐµÐ´ÐµÐ» Ð² Ð³Ñ€ÑƒÐ¿Ð¿Ðµ ({n} {var} Ð¼Ð°ÐºÑÐ¸Ð¼ÑƒÐ¼)', ['items','item']],
+      noneSelectedText : 'Ничего не выбрано',
+        noneResultsText : 'Не нейдено Совпадений',
+          countSelectedText : 'Выбрано {0} из {1}',
+      maxOptionsText: ['Достигнут предел ({n} {var} максимум)', 'Достигнут предел в группе ({n} {var} максимум)', ['items','item']],
       width: false,
       container: false,
       hideDisabled: false,
