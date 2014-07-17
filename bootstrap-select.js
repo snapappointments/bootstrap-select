492bd01bc93b1fdf048e53434b3e827e22186a62
danielmariz <danielcsmariz@gmail.com>
2014-07-16 22:53:26 -0300

    adding placeholder on input search

--- bootstrap-select.js
+++ bootstrap-select.js
@@ -86,7 +86,7 @@
             var inputGroup = this.$element.parent().hasClass('input-group') ? ' input-group-btn' : '';
             var autofocus = this.autofocus ? ' autofocus' : '';
             var header = this.options.header ? '<div class="popover-title"><button type="button" class="close" aria-hidden="true">&times;</button>' + this.options.header + '</div>' : '';
-            var searchbox = this.options.liveSearch ? '<div class="bootstrap-select-searchbox"><input type="text" class="input-block-level form-control" autocomplete="off" /></div>' : '';
+            var searchbox = this.options.liveSearch ? '<div class="bootstrap-select-searchbox"><input type="text" class="input-block-level form-control" autocomplete="off"  placeholder="'+(this.options.liveSearchTitle || '')+'" /></div>' : '';
             var actionsbox = this.options.actionsBox ? '<div class="bs-actionsbox">' +
                                 '<div class="btn-group btn-block">' +
                                     '<button class="actions-btn bs-select-all btn btn-sm btn-default">' +
