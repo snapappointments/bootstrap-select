4ac96e52399a68cf77ea984b1383dd103bc35707
Nerone21 <nerone21@hotmail.com>
2014-08-01 09:46:09 +0200

    Create defaults-fr-FR.js

---
+++ js/i18n/defaults-fr-FR.js
@@ -0,0 +1,29 @@
+/*
+* Translated default messages for bootstrap-select.
+* Locale: FR (France)
+* Region: EU
+*/
+(function ($) {
+  $.fn.selectpicker.defaults = {
+    style: 'btn-default',
+    size: 'auto',
+    title: null,
+    selectedTextFormat: 'values',
+    noneSelectedText: 'Aucune(s) sélection(s)',
+    noneResultsText: 'Aucun(s) résultat(s)',
+    countSelectedText: '{0} sur {1} sélection(s)',
+    maxOptionsText: ['Limite atteinte ({n} {var} max)', 'Limite du groupe atteinte ({n} {var} max)', ['položek', 'položka']],
+    width: false,
+    container: false,
+    hideDisabled: false,
+    showSubtext: false,
+    showIcon: true,
+    showContent: true,
+    dropupAuto: true,
+    header: false,
+    liveSearch: false,
+    multipleSeparator: ', ',
+    iconBase: 'glyphicon',
+    tickIcon: 'glyphicon-ok'
+  };
+}(jQuery));
