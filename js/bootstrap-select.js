4a9cd78eedfcefd0bfdf4cb16776ce575c1cf392
t0xicCode <xavier.l@t0xic.co.de>
2014-08-18 22:16:16 -0400

    Bumped to v1.6.2

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -39,7 +39,7 @@
     this.init();
   };
 
-  Selectpicker.VERSION = '1.6.1';
+  Selectpicker.VERSION = '1.6.2';
 
   // part of this is duplicated in i18n/defaults-en_US.js. Make sure to update both.
   Selectpicker.DEFAULTS = {
