f2a43be1a2ce4d62474a598c4e00100ad13f63e6
t0xicCode <xavier.l@t0xic.co.de>
2014-12-28 12:20:29 -0500

    Updated devdependencies

--- js/i18n/defaults-tr_TR.js
+++ js/i18n/defaults-tr_TR.js
@@ -1,23 +1,23 @@
 /*
-* Translated default messages for bootstrap-select.
-* Locale: TR (Turkey)
-* Region: TR (Europe)
-*/
+ * Translated default messages for bootstrap-select.
+ * Locale: TR (Turkey)
+ * Region: TR (Europe)
+ */
 (function ($) {
-$.fn.selectpicker.defaults = {
-noneSelectedText: 'Hiçbiri seçilmedi',
-noneResultsText: 'Hiçbir sonuç bulunamadı {0}',
-countSelectedText: function (numSelected, numTotal) {
-return (numSelected == 1) ? "{0} öğe seçildi" : "{0} öğe seçildi";
-},
-maxOptionsText: function (numAll, numGroup) {
-var arr = [];
-arr[0] = (numAll == 1) ? 'Limit aşıldı (maksimum {n} sayıda öğe )' : 'Limit aşıldı (maksimum {n} sayıda öğe)';
-arr[1] = (numGroup == 1) ? 'Grup limiti aşıldı (maksimum {n} sayıda öğe)' : 'Grup limiti aşıldı (maksimum {n} sayıda öğe)';
-return arr;
-},
-selectAllText: 'Hepsini seç',
-deselectAllText: 'Hiçbirini seçme',
-multipleSeparator: ', '
-};
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'Hiçbiri seçilmedi',
+    noneResultsText: 'Hiçbir sonuç bulunamadı {0}',
+    countSelectedText: function (numSelected, numTotal) {
+      return (numSelected == 1) ? "{0} öğe seçildi" : "{0} öğe seçildi";
+    },
+    maxOptionsText: function (numAll, numGroup) {
+      var arr = [];
+      arr[0] = (numAll == 1) ? 'Limit aşıldı (maksimum {n} sayıda öğe )' : 'Limit aşıldı (maksimum {n} sayıda öğe)';
+      arr[1] = (numGroup == 1) ? 'Grup limiti aşıldı (maksimum {n} sayıda öğe)' : 'Grup limiti aşıldı (maksimum {n} sayıda öğe)';
+      return arr;
+    },
+    selectAllText: 'Hepsini seç',
+    deselectAllText: 'Hiçbirini seçme',
+    multipleSeparator: ', '
+  };
 }(jQuery));
