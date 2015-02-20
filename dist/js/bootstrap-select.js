22c316a8a173bc373b0d53a1a2bc4719e060450d
Anush Ramani <anush@xola.com>
2015-02-20 11:16:10 +0530

    Ran grunt dist

--- dist/js/bootstrap-select.js
+++ dist/js/bootstrap-select.js
@@ -231,6 +231,7 @@
   Selectpicker.DEFAULTS = {
     noneSelectedText: 'Nothing selected',
     noneResultsText: 'No results matched {0}',
+    createItemText: 'Create: {0}',
     countSelectedText: function (numSelected, numTotal) {
       return (numSelected == 1) ? "{0} item selected" : "{0} items selected";
     },
@@ -267,7 +268,8 @@
     maxOptions: false,
     mobile: false,
     selectOnTab: false,
-    dropdownAlignRight: false
+    dropdownAlignRight: false,
+    createItem: false
   };
 
   Selectpicker.prototype = {
@@ -1027,7 +1029,8 @@
             if (!!no_results.parent().length) {
               no_results.remove();
             }
-            no_results.html(that.options.noneResultsText.replace('{0}', '"' + htmlEscape(that.$searchbox.val()) + '"')).show();
+            var text = that.options.createItem ? that.options.createItemText : that.options.noneResultsText;
+            no_results.html(text.replace('{0}', '"' + htmlEscape(that.$searchbox.val()) + '"')).show();
             that.$menu.find('li').last().after(no_results);
           } else if (!!no_results.parent().length) {
             no_results.remove();
@@ -1283,7 +1286,12 @@
           // Prevent screen from scrolling if the user hit the spacebar
           e.preventDefault();
         } else if (!/(32)/.test(e.keyCode.toString(10))) {
-          that.$menu.find('.active a').click();
+          var active = that.$menu.find('.active');
+          if(active.hasClass('no-results') && that.options.createItem) {
+            that.createItem(that.$searchbox.val());
+          } else {
+            that.$menu.find('.active a').click();
+          }
           $this.focus();
         }
         $(document).data('keycount', 0);
@@ -1293,6 +1301,14 @@
         that.$menu.parent().removeClass('open');
         that.$button.focus();
       }
+    },
+
+    createItem: function(val) {
+      this.$element.append($('<option value="' + val + '">' + val + '</option>'));
+      this.$newElement.removeClass('open');
+      this.refresh();
+      this.val(val);
+      this.$element.change();
     },
 
     mobile: function () {
