009f92f93f930b5c4bf62af05e887bb4487017d2
vsn4ik <vsn4ik@gmail.com>
2015-02-23 14:19:22 +0300

    Fix mobile with input-group and JS refactoring.

--- js/i18n/defaults-ru_RU.js
+++ js/i18n/defaults-ru_RU.js
@@ -9,6 +9,7 @@
     noneResultsText: 'Совпадений не найдено {0}',
     countSelectedText: 'Выбрано {0} из {1}',
     maxOptionsText: ['Достигнут предел ({n} {var} максимум)', 'Достигнут предел в группе ({n} {var} максимум)', ['items', 'item']],
+    doneButtonText: 'Закрыть',
     multipleSeparator: ', '
   };
 })(jQuery);
