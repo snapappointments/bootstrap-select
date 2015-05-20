6bef69b0d4d18514d7ebdbb29d867153cb5c3596
Casey Holzer <casey17648@gmail.com>
2015-05-20 12:45:24 -0600

    Object.keys polyfill
    Fix #769. Fix #759.

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -108,6 +108,23 @@
       }
     }());
   }
+
+  if (!Object.keys) {
+    Object.keys = function (
+      o, // object
+      k, // key
+      r  // result array
+      ){
+      // initialize object and result
+      r=[];
+      // iterate over object keys
+      for (k in o) 
+          // fill result array with non-prototypical keys
+        r.hasOwnProperty.call(o, k) && r.push(k);
+      // return result
+      return r
+    };
+  }
   //</editor-fold>
 
   // Case insensitive contains search
