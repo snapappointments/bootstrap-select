07419818ec3c0e52981f75c5837dba175b6329e7
Casey Holzer <casey17648@gmail.com>
2015-12-07 13:16:01 -0700

    v1.8.0 bump

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -264,7 +264,7 @@
     this.init();
   };
 
-  Selectpicker.VERSION = '1.7.7';
+  Selectpicker.VERSION = '1.8.0';
 
   // part of this is duplicated in i18n/defaults-en_US.js. Make sure to update both.
   Selectpicker.DEFAULTS = {
