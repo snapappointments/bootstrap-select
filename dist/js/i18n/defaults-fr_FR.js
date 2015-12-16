fd5572a51e67495f33c3d78965331d4ced0d4cd9
Casey Holzer <casey17648@gmail.com>
2015-12-16 12:48:55 -0700

    Release v1.9.2

--- dist/js/i18n/defaults-fr_FR.js
+++ dist/js/i18n/defaults-fr_FR.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap-select v1.8.1 (http://silviomoreto.github.io/bootstrap-select)
+ * Bootstrap-select v1.9.2 (http://silviomoreto.github.io/bootstrap-select)
  *
  * Copyright 2013-2015 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
@@ -23,15 +23,15 @@
 
 (function ($) {
   $.fn.selectpicker.defaults = {
-    noneSelectedText: 'Aucune s&eacute;lection',
-    noneResultsText: 'Aucun r&eacute;sultat pour {0}',
+    noneSelectedText: 'Aucune sélection',
+    noneResultsText: 'Aucun résultat pour {0}',
     countSelectedText: function (numSelected, numTotal) {
-      return (numSelected > 1) ? "{0} &eacute;l&eacute;ments s&eacute;lectionn&eacute;s" : "{0} &eacute;l&eacute;ment s&eacute;lectionn&eacute;";
+      return (numSelected > 1) ? "{0} éléments sélectionnés" : "{0} élément sélectionné";
     },
     maxOptionsText: function (numAll, numGroup) {
       return [
-        (numAll > 1) ? 'Limite atteinte ({n} &eacute;l&eacute;ments max)' : 'Limite atteinte ({n} &eacute;l&eacute;ment max)',
-        (numGroup > 1) ? 'Limite du groupe atteinte ({n} &eacute;l&eacute;ments max)' : 'Limite du groupe atteinte ({n} &eacute;l&eacute;ment max)'
+        (numAll > 1) ? 'Limite atteinte ({n} éléments max)' : 'Limite atteinte ({n} élément max)',
+        (numGroup > 1) ? 'Limite du groupe atteinte ({n} éléments max)' : 'Limite du groupe atteinte ({n} élément max)'
       ];
     },
     multipleSeparator: ', '
