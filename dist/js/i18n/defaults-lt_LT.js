25b7e1398980ccf86c56d069acbf56597be71ac3
Casey Holzer <casey17648@gmail.com>
2016-02-17 11:29:02 -0700

    Deployed a1b3d49 with MkDocs version: 0.14.0

--- dist/js/i18n/defaults-lt_LT.js
+++ dist/js/i18n/defaults-lt_LT.js
@@ -23,19 +23,19 @@
 
 (function ($) {
   $.fn.selectpicker.defaults = {
-    noneSelectedText: 'Odaberite stavku',
-    noneResultsText: 'Nema rezultata pretrage {0}',
+    noneSelectedText: 'Niekas nepasirinkta',
+    noneResultsText: 'Niekas nesutapo su {0}',
     countSelectedText: function (numSelected, numTotal) {
-      return (numSelected == 1) ? "{0} stavka selektirana" : "{0} stavke selektirane";
+      return (numSelected == 1) ? "{0} elementas pasirinktas" : "{0} elementai(-ų) pasirinkta";
     },
     maxOptionsText: function (numAll, numGroup) {
       return [
-        (numAll == 1) ? 'Limit je postignut ({n} stvar maximalno)' : 'Limit je postignut ({n} stavke maksimalno)',
-        (numGroup == 1) ? 'Grupni limit je postignut ({n} stvar maksimalno)' : 'Grupni limit je postignut ({n} stavke maksimalno)'
+        (numAll == 1) ? 'Pasiekta riba ({n} elementas daugiausiai)' : 'Riba pasiekta ({n} elementai(-ų) daugiausiai)',
+        (numGroup == 1) ? 'Grupės riba pasiekta ({n} elementas daugiausiai)' : 'Grupės riba pasiekta ({n} elementai(-ų) daugiausiai)'
       ];
     },
-    selectAllText: 'Selektiraj sve',
-    deselectAllText: 'Deselektiraj sve',
+    selectAllText: 'Pasirinkti visus',
+    deselectAllText: 'Atmesti visus',
     multipleSeparator: ', '
   };
 })(jQuery);
