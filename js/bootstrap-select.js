snapappointments/bootstrap-select

commit ead67e5bef03ac4c9baa4277ba030b1cb4bdedb4

fix #1489

js/bootstrap-select.js
@@ -1614,9 +1614,7 @@
       if (that.options.liveSearch) {
         if (/(^9$|27)/.test(e.keyCode.toString(10)) && isActive && that.$menu.find('.active').length === 0) {
           e.preventDefault();
-          that.$menu.parent().removeClass('open');
-          if (that.options.container) that.$newElement.removeClass('open');
-          that.$button.focus();
+          that.$button.click().focus();
         }
         // $items contains li elements when liveSearch is enabled
         $items = $('[role="listbox"] li' + selector, $parent);

commit 3044fa833adb39e353a61c2103b7a73e78eb9fc4

fix selectOnTab with liveSearch enabled. broken in ead67e5 (#1489)

js/bootstrap-select.js
@@ -1637,7 +1637,8 @@
         if (/(^9$|27)/.test(e.keyCode.toString(10)) && isActive) {
           e.preventDefault();
           e.stopPropagation();
-          that.$button.click().focus();
+          that.$menuInner.click();
+          that.$button.focus();
         }
         // $items contains li elements when liveSearch is enabled
         $items = $('[role="listbox"] li' + selector, $parent);