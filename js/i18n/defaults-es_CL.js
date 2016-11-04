ad70979ab083dbb52cc436ef885ef582014b1d7d
Casey Holzer <casey17648@gmail.com>
2016-11-04 15:33:26 -0600

    add select/deselectAllText translations to es_CL (#1529)

--- js/i18n/defaults-es_CL.js
+++ js/i18n/defaults-es_CL.js
@@ -9,6 +9,8 @@
     noneResultsText: 'No hay resultados {0}',
     countSelectedText: 'Seleccionados {0} de {1}',
     maxOptionsText: ['Límite alcanzado ({n} {var} max)', 'Límite del grupo alcanzado({n} {var} max)', ['elementos', 'element']],
-    multipleSeparator: ', '
+    multipleSeparator: ', ',
+    selectAllText: 'Seleccionar Todos',
+    deselectAllText: 'Desmarcar Todos'
   };
 })(jQuery);
