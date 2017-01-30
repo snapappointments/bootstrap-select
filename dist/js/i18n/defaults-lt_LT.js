8f628848419aaab3672397517b3114d13bf0e6e5
Casey Holzer <casey17648@gmail.com>
2017-01-30 13:28:23 -0700

    Deployed 4eb040b with MkDocs version: 0.15.3

--- dist/js/i18n/defaults-lt_LT.js
+++ dist/js/i18n/defaults-lt_LT.js
@@ -1,7 +1,7 @@
 /*!
- * Bootstrap-select v1.12.1 (http://silviomoreto.github.io/bootstrap-select)
+ * Bootstrap-select v1.12.2 (http://silviomoreto.github.io/bootstrap-select)
  *
- * Copyright 2013-2016 bootstrap-select
+ * Copyright 2013-2017 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
  */
 
