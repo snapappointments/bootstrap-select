0f6256c3fca44010dd8bff2cb574be6b2b2b102e
szmalec <szmalec@poczta.fm>
2017-03-20 11:04:33 +0100

    Update defaults-pl_PL.js
    Fixed translation key names (selectAll -> selectAllText, deselectAll -> deselectAllText).

--- js/i18n/defaults-pl_PL.js
+++ js/i18n/defaults-pl_PL.js
@@ -9,8 +9,8 @@
     noneResultsText: 'Brak wyników wyszukiwania {0}',
     countSelectedText: 'Zaznaczono {0} z {1}',
     maxOptionsText: ['Osiągnięto limit ({n} {var} max)', 'Limit grupy osiągnięty ({n} {var} max)', ['elementy', 'element']],
-    selectAll: 'Zaznacz wszystkie',
-    deselectAll: 'Odznacz wszystkie',
+    selectAllText: 'Zaznacz wszystkie',
+    deselectAllText: 'Odznacz wszystkie',
     multipleSeparator: ', '
   };
 })(jQuery);
