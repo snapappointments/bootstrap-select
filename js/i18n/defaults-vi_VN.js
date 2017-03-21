525e41d8fd923eab22aa71b885413e408089de0e
Phongdatgl <phongdatgl@gmail.com>
2017-03-21 13:21:56 +0700

    Add Vietnamese language

---
+++ js/i18n/defaults-vi_VN.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for bootstrap-select.
+ * Locale: EN (English)
+ * Region: US (United States)
+ */
+(function ($) {
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'Chưa chọn',
+    noneResultsText: 'Không có kết quả {0}',
+    countSelectedText: function (numSelected, numTotal) {
+      return "{0} mục đã chọn";
+    },
+    maxOptionsText: function (numAll, numGroup) {
+      return [
+        'Không thể chọn (giới hạn {n} mục)',
+        'Không thể chọn (giới hạn {n} mục)'
+      ];
+    },
+    selectAllText: 'Chọn tất cả',
+    deselectAllText: 'Bỏ chọn',
+    multipleSeparator: ', '
+  };
+})(jQuery);
