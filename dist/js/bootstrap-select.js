095aaf5981645a9e7223f762bd187734683da3ca
scriptype <scriptyper@gmail.com>
2017-09-27 14:39:58 +0400

    Rebuild after removing console

--- dist/js/bootstrap-select.js
+++ dist/js/bootstrap-select.js
@@ -1361,8 +1361,6 @@
             }
           }
 
-          console.log('that', that)
-
           if (!that.multiple || (that.multiple && that.options.maxOptions === 1)) {
             that.$button.focus();
           } else if (that.options.liveSearch && !that.options.mobile) {
