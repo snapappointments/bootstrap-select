89f4d3efa04ea12a0fb05e881341d64c6f6de1be
Durgesh Sonawane <dur4791@gmail.com>
2017-12-03 16:55:09 +0530

    aria-labelledby attribute appended to the dropdown for improving the accessibility for the JAWS user (#1862)

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -499,7 +499,7 @@
           : '';
       var drop =
           '<div class="btn-group bootstrap-select' + showTick + inputGroup + '">' +
-          '<button type="button" class="' + this.options.styleBase + ' dropdown-toggle" data-toggle="dropdown"' + autofocus + ' role="button">' +
+          '<button type="button" class="' + this.options.styleBase + ' dropdown-toggle" data-toggle="dropdown"' + autofocus + ' role="button" aria-labelledby="'+this.$element.attr("aria-labelledby")+'">' +
           '<span class="filter-option pull-left"></span>&nbsp;' +
           '<span class="bs-caret">' +
           this.options.template.caret +
