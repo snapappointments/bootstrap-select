1e51c87b992d20f025b19f7bf573b5b3d7ed6c2b
Casey Holzer <casey17648@gmail.com>
2017-12-14 17:15:14 -0700

    remove version from composer.json (recommended)

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,7 +218,6 @@ module.exports = function (grunt) {
           prefix: '[\'"]?version[\'"]?:[ "\']*'
         },
         src: [
-          'composer.json',
           'docs/mkdocs.yml',
           'package.json'
         ],
