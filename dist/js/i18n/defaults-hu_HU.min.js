c19e3da573dcb006ede4ad74b615f505b2ea7aa1
Casey Holzer <caseyh@snapappointments.com>
2018-10-15 15:54:55 -0700

    Deployed 0a79b80 with MkDocs version: 0.17.5

--- dist/js/i18n/defaults-hu_HU.min.js
+++ dist/js/i18n/defaults-hu_HU.min.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap-select v1.13.2 (https://developer.snapappointments.com/bootstrap-select)
+ * Bootstrap-select v1.13.3 (https://developer.snapappointments.com/bootstrap-select)
  *
  * Copyright 2012-2018 SnapAppointments, LLC
  * Licensed under MIT (https://github.com/snapappointments/bootstrap-select/blob/master/LICENSE)
