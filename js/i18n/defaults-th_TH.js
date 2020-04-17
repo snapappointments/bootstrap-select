f184747117f553d4e648e987811dfd9eae692dc8
heemza <6562562+heemza@users.noreply.github.com>
2020-04-18 03:59:43 +0700

    Add translations for Thai language (#2340)

---
+++ js/i18n/defaults-th_TH.js
@@ -0,0 +1,17 @@
+/*
+ * Translated default messages for bootstrap-select.
+ * Locale: TH (Thai)
+ * Region: TH (Thailand)
+ * Author: Ekkachai Sohmontha
+ */
+(function ($) {
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'ไม่ได้เลือกอะไรเลย',
+    noneResultsText: 'ไม่มีผลลัพธ์ที่ตรงกัน {0}',
+    countSelectedText: '{0} รายการที่เลือก',
+    maxOptionsText: ['เกินจำนวนที่กำหนด (สูงสุด {n} รายการ)', 'เกินจำนวนที่กำหนด (สูงสุด {n} กลุ่ม)'],
+    selectAllText: 'เลือกทั้งหมด',
+    deselectAllText: 'ไม่เลือกทั้งหมด',
+    multipleSeparator: ', '
+  };
+})(jQuery);
