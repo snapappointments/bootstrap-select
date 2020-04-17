54c7722dab23ea3de28d705fc1d037f82b4289fd
Casey Holzer <caseyh@snapappointments.com>
2020-04-17 16:46:03 -0600

    fix whitespace

--- js/i18n/defaults-sr_SP.js
+++ js/i18n/defaults-sr_SP.js
@@ -4,20 +4,20 @@
  * Region: SR (Republic of Serbia)
  */
 (function ($) {
-    $.fn.selectpicker.defaults = {
-        noneSelectedText: 'Izaberite',
-        noneResultsText: 'Nema rezultata za {0}',
-        countSelectedText: function (numSelected, numTotal) {
-            return (numSelected == 1) ? '{0} izabrana' : '{0} izabrane';
-        },
-        maxOptionsText: function (numAll, numGroup) {
-            return [
-                (numAll == 1) ? 'Limit je dostignut ({n} stvar maximalno)' : 'Limit je dostignut ({n} stavke maksimalno)',
-                (numGroup == 1) ? 'Grupni limit je dostignut ({n} stvar maksimalno)' : 'Grupni limit je dostignut ({n} stavke maksimalno)'
-            ];
-        },
-        selectAllText: 'Izaberi sve',
-        deselectAllText: 'Obrisi sve',
-        multipleSeparator: ', '
-    };
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'Izaberite',
+    noneResultsText: 'Nema rezultata za {0}',
+    countSelectedText: function (numSelected, numTotal) {
+      return (numSelected == 1) ? '{0} izabrana' : '{0} izabrane';
+    },
+    maxOptionsText: function (numAll, numGroup) {
+      return [
+        (numAll == 1) ? 'Limit je dostignut ({n} stvar maximalno)' : 'Limit je dostignut ({n} stavke maksimalno)',
+        (numGroup == 1) ? 'Grupni limit je dostignut ({n} stvar maksimalno)' : 'Grupni limit je dostignut ({n} stavke maksimalno)'
+      ];
+    },
+    selectAllText: 'Izaberi sve',
+    deselectAllText: 'Obrisi sve',
+    multipleSeparator: ', '
+  };
 })(jQuery);
