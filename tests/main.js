249acfcd04aa40a95fa9c6683c226bbb052fe876
Nicolas CARPi <nico-git@deltablot.email>
2021-11-27 01:50:43 +0100

    improve test pages
    * remove bootstrap3 test page
    * remove duplicated js code
    * improve js code for test pages
    * update bootstrap5 to latest version
    * update jquery to latest version

---
+++ tests/main.js
@@ -0,0 +1,42 @@
+/**
+ * Bootstrap-select test page js
+ */
+
+/**
+ * Add options to a select element
+ */
+const createOptions = (elemId, num) => {
+  // should be a select
+  const element = document.getElementById(elemId);
+  for (let i = 1; i <= num; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.text = `Option ${i}`;
+    element.appendChild(option);
+  }
+}
+
+// we need to  include the options before the document is ready and bootstrap select starts working
+document.addEventListener('readystatechange', () => {
+  // "interactive" is when all is loaded but bootstrap-select is not yet activated
+  if (document.readyState === 'interactive') {
+    ['number', 'number-multiple', 'number2', 'number2-multiple'].forEach(elemId => createOptions(elemId, 30));
+
+    const mySelect = $('#first-disabled2');
+
+    $('#special').on('click', function () {
+      mySelect.find('option:selected').prop('disabled', true);
+      mySelect.selectpicker('refresh');
+    });
+
+    $('#special2').on('click', function () {
+      mySelect.find('option:disabled').prop('disabled', false);
+      mySelect.selectpicker('refresh');
+    });
+
+    $('#basic2').selectpicker({
+      liveSearch: true,
+      maxOptions: 1
+    });
+  }
+});
