dd3aab2e43bc329b9623670086aed8e61f7a3739
Casey Holzer <casey.holzer@hospiq.com>
2022-02-20 09:21:12 -0700

    initial Cypress tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,28 @@
+import {v4 as uuidv4} from 'uuid';
+
+Cypress.Commands.add('selectpicker', ({options, selector, html}) => {
+    let id;
+    if (!selector) {
+        id = uuidv4();
+        selector = `#${id}`;
+    }
+
+    cy.window().then(window => {
+        const $ = window.$;
+
+        if (html) {
+            const $select = $(html);
+
+            if (id && !$select[0].id) {
+                $select.attr('id', id);
+            }
+            $('body').append($select);
+        } else if (id) {
+            $('body').append(`<select id="${id}" />`);
+        }
+
+        $(selector).selectpicker(options);
+    });
+
+    return cy.get(selector);
+});
