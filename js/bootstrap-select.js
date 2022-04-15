e2b71a289299acd67c2d502c397596f0018d4a1f
Brian Larson <brian.larson@northwestern.edu>
2022-04-15 09:39:31 -0500

    aria assessibility fixes found using axe devtools

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -1205,7 +1205,7 @@
             (this.options.display === 'static' ? 'data-display="static"' : '') +
             Selector.DATA_TOGGLE +
             autofocus +
-            ' role="combobox" aria-owns="' +
+            ' aria-owns="' +
             this.selectId +
             '" aria-haspopup="listbox" aria-expanded="false">' +
             '<div class="filter-option">' +
