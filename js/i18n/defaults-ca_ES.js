9d02fc071d1d354f672f981ccb0bed25393b4735
Santiago L <santiago@ribaguifi.com>
2022-08-02 15:32:53 +0200

    add i18n-ca_ES (catalan translation) (#2692)

---
+++ js/i18n/defaults-ca_ES.js
@@ -0,0 +1,16 @@
+/*
+ * Translated default messages for bootstrap-select.
+ * Locale: CA (Catalan)
+ * Region: ES (Spain)
+ */
+(function ($) {
+  $.fn.selectpicker.defaults = {
+    noneSelectedText: 'No hi ha selecció',
+    noneResultsText: 'No hi ha resultats {0}',
+    countSelectedText: 'Seleccionats {0} de {1}',
+    maxOptionsText: ['Límit assolit ({n} {var} max)', 'Límit del grup assolit({n} {var} max)', ['elements', 'element']],
+    multipleSeparator: ', ',
+    selectAllText: 'Seleccionar Tots',
+    deselectAllText: 'Desmarcar Tots'
+  };
+})(jQuery);
