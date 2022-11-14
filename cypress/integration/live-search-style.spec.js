5c2bf5267ad87a6878a33e36ccd348e41d9fe550
Jacques-Yves Bleau <jybleau@panache.media>
2022-11-14 15:20:47 -0500

    #2792 refactor style test title formatting

--- cypress/integration/live-search-style.spec.js
+++ cypress/integration/live-search-style.spec.js
@@ -60,37 +60,31 @@ describe('Filter with search style', () => {
     });
 
     selectpickerConfigs.forEach(({ filter, config, title }) => {
-        if (filter === 'containsAll') {
-            doTests(title + ' can find options containing all the search words');
-        } else {
-            doTests(title + ' can find options containing the whole search string');
-        }
+        const canTitle = `${title} can find options containing ${(filter === 'containsAll' ? 'all the search words' : 'the whole search string')}`
 
-        function doTests(can) {
-            it(can, () => {
-                cy.selectpicker(config).then(($select) => {
-                    const button = `[data-id="${$select[0].id}"]`;
-                    cy.get(button).click();
+        it(canTitle, () => {
+            cy.selectpicker(config).then(($select) => {
+                const button = `[data-id="${$select[0].id}"]`;
+                cy.get(button).click();
 
-                    cy.get('input').type('option 4');
+                cy.get('input').type('option 4');
 
-                    cy.get('.dropdown-menu').find('li')
-                        .then(($el) => {
-                            if (filter === 'containsAll') {
-                                // expecting to find 2 items with values: Option 4 and Option 14.
-                                // Those are the only 2 items in our test array containing the "Option" and the "4" words.
-                                expect($el).to.have.length(2);
-                                expect($el[0].textContent).to.equal('Option 4');
-                                expect($el[1].textContent).to.equal('Option 14');
-                            } else {
-                                // default "contains". Look for the complete search string "as is"; no word separation is expected.
-                                // There is only 1 option in our test array containing the whole "option 4" search string as is.
-                                expect($el).to.have.length(1);
-                                expect($el[0].textContent).to.equal('Option 4');
-                            }
-                        });
-                });
+                cy.get('.dropdown-menu').find('li')
+                    .then(($el) => {
+                        if (filter === 'containsAll') {
+                            // expecting to find 2 items with values: Option 4 and Option 14.
+                            // Those are the only 2 items in our test array containing the "Option" and the "4" words.
+                            expect($el).to.have.length(2);
+                            expect($el[0].textContent).to.equal('Option 4');
+                            expect($el[1].textContent).to.equal('Option 14');
+                        } else {
+                            // default "contains". Look for the complete search string "as is"; no word separation is expected.
+                            // There is only 1 option in our test array containing the whole "option 4" search string as is.
+                            expect($el).to.have.length(1);
+                            expect($el[0].textContent).to.equal('Option 4');
+                        }
+                    });
             });
-        }
+        });
     });
 });
