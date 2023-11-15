c4fd4a40aa36e1399131a352414f4797bd9e06ba
Nick Lai <resxc13579@gmail.com>
2023-11-15 16:13:03 +0800

    chore: enable the "Run All Specs" UI feature

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // Enables the "Run All Specs" UI feature, allowing the execution of
+    // multiple specs sequentially.
+    experimentalRunAllSpecs: true,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
